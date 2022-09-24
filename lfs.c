238f11391011d3807664ae2ee85031903cea4af2
Helienzo <lucas.wennerholm@gmail.com>
2022-09-24 12:50:05 +0200

    TMP onging work wiht ctz

--- lfs.c
+++ lfs.c
@@ -2688,101 +2688,195 @@ static int lfs_ctz_find(lfs_t *lfs,
 }
 
 #ifndef LFS_READONLY
-static int lfs_ctz_extend(lfs_t *lfs,
+static int ctz_extend_alloc(lfs_t *lfs,
         lfs_cache_t *pcache, lfs_cache_t *rcache,
         lfs_block_t head, lfs_size_t size,
-        lfs_block_t *block, lfs_off_t *off) {
-    while (true) {
+        lfs_block_t *block, lfs_off_t *off, lfs_block_t *nblock) {
+
         // go ahead and grab a block
-        lfs_block_t nblock;
-        int err = lfs_alloc(lfs, &nblock);
+        int err = lfs_alloc(lfs, nblock);
         if (err) {
             return err;
         }
 
-        {
-            err = lfs_bd_erase(lfs, nblock);
+        // call erase
+        return ctz_extend_block_erase(lfs, pcache, rcache, head, size, block, off, nblock);
+}
+
+
+static int ctz_extend_done(lfs_t *lfs,
+        lfs_cache_t *pcache, lfs_cache_t *rcache,
+        lfs_block_t head, lfs_size_t size,
+        lfs_block_t *block, lfs_off_t *off,  lfs_block_t *nblock) {
+    if (file->action_complete_cb == NULL) {
+        // Do a blocking call
+        int err = lfs->flushedwrite_next(lfs, file, data, nsize);
+        return err;
+    }
+    else {
+        // Do a non blocking call
+        return LFS_ERR_OK;
+    }
+
+}
+static int ctz_extend_nothing_to_write(lfs_t *lfs,
+        lfs_cache_t *pcache, lfs_cache_t *rcache,
+        lfs_block_t head, lfs_size_t size,
+        lfs_block_t *block, lfs_off_t *off,  lfs_block_t *nblock) {
+    // Next is copy_out
+    lfs->ctz_extend_next = ctz_extend_copy_out;
+
+    if (size == 0) {
+        *block = nblock;
+        *off = 0;
+        // If nothing to write we are done
+        lfs->ctz_extend_next = ctz_extend_done;
+    }
+
+    return lfs->ctz_extend_next(lfs, pcache, rcache, head, size, block, off, nblock);
+}
+
+static int ctz_extend_relocate(lfs_t *lfs,
+        lfs_cache_t *pcache, lfs_cache_t *rcache,
+        lfs_block_t head, lfs_size_t size,
+        lfs_block_t *block, lfs_off_t *off,  lfs_block_t *nblock) {
+
+        LFS_DEBUG("Bad block at 0x%"PRIx32, nblock);
+        // just clear cache and try a new block
+        lfs_cache_drop(lfs, pcache);
+        return ctz_extend_alloc(lfs, pcache, rcache, head, size, block, off, nblock);
+}
+
+static int ctz_extend_block_erase(lfs_t *lfs,
+        lfs_cache_t *pcache, lfs_cache_t *rcache,
+        lfs_block_t head, lfs_size_t size,
+        lfs_block_t *block, lfs_off_t *off,  lfs_block_t *nblock) {
+
+        // Prepare the next call
+        lfs->ctz_extend_next = ctz_extend_nothing_to_write;
+
+        // erase block
+        int err = lfs_bd_erase(lfs, nblock);
+
+        // corrupt block block goto relocate (can only happen in blocking mode)
+        if (err) {
+            if (err == LFS_ERR_CORRUPT) {
+                // Set next action function
+                lfs->ctz_extend_next = ctz_extend_relocate;
+            }
+            else {
+                // TODO If it not a corrupt error we must propagete error
+                return err;
+            }
+        }
+        return lfs->ctz_extend_next(lfs, pcache, rcache, head, size, block, off, nblock);
+}
+
+static int ctz_extend_append(lfs_t *lfs,
+        lfs_cache_t *pcache, lfs_cache_t *rcache,
+        lfs_block_t head, lfs_size_t size,
+        lfs_block_t *block, lfs_off_t *off,  lfs_block_t *nblock) {
+
+        // prepare next state
+        lfs->ctz_extend_next = ctz_extend_done;
+        // append block
+        index += 1;
+        lfs_size_t skips = lfs_ctz(index) + 1;
+        lfs_block_t nhead = head;
+        bool relocate = 0;
+        for (lfs_off_t i = 0; i < skips; i++) {
+            nhead = lfs_tole32(nhead);
+            err = lfs_bd_prog(lfs, pcache, rcache, true,
+                    nblock, 4*i, &nhead, 4);
+            nhead = lfs_fromle32(nhead);
             if (err) {
                 if (err == LFS_ERR_CORRUPT) {
-                    goto relocate;
+                    // This was a goto relocate, stop the for loop
+                    relocate = 1;
+                    break;
                 }
                 return err;
             }
 
-            if (size == 0) {
-                *block = nblock;
-                *off = 0;
-                return 0;
-            }
-
-            lfs_size_t noff = size - 1;
-            lfs_off_t index = lfs_ctz_index(lfs, &noff);
-            noff = noff + 1;
-
-            // just copy out the last block if it is incomplete
-            if (noff != lfs->cfg->block_size) {
-                for (lfs_off_t i = 0; i < noff; i++) {
-                    uint8_t data;
-                    err = lfs_bd_read(lfs,
-                            NULL, rcache, noff-i,
-                            head, i, &data, 1);
-                    if (err) {
-                        return err;
-                    }
-
-                    err = lfs_bd_prog(lfs,
-                            pcache, rcache, true,
-                            nblock, i, &data, 1);
-                    if (err) {
-                        if (err == LFS_ERR_CORRUPT) {
-                            goto relocate;
-                        }
-                        return err;
-                    }
-                }
-
-                *block = nblock;
-                *off = noff;
-                return 0;
-            }
-
-            // append block
-            index += 1;
-            lfs_size_t skips = lfs_ctz(index) + 1;
-            lfs_block_t nhead = head;
-            for (lfs_off_t i = 0; i < skips; i++) {
-                nhead = lfs_tole32(nhead);
-                err = lfs_bd_prog(lfs, pcache, rcache, true,
-                        nblock, 4*i, &nhead, 4);
+            if (i != skips-1) {
+                err = lfs_bd_read(lfs,
+                        NULL, rcache, sizeof(nhead),
+                        nhead, 4*i, &nhead, sizeof(nhead));
                 nhead = lfs_fromle32(nhead);
                 if (err) {
-                    if (err == LFS_ERR_CORRUPT) {
-                        goto relocate;
-                    }
+                    return err;
+                }
+            }
+        }
+
+        if (relocate) {
+            lfs->ctz_extend_next = ctz_extend_relocate;
+        } else {
+            // If we get here the block allocation was successfull
+            *block = nblock;
+            *off = 4*skips;
+        }
+
+        return lfs->ctz_extend_next(lfs, pcache, rcache, head, size, block, off, nblock);
+}
+
+static int ctz_extend_copy_out(lfs_t *lfs,
+        lfs_cache_t *pcache, lfs_cache_t *rcache,
+        lfs_block_t head, lfs_size_t size,
+        lfs_block_t *block, lfs_off_t *off,  lfs_block_t *nblock) {
+
+        // Prepare the next call
+        lfs->ctz_extend_next = ctz_extend_append;
+
+        lfs_size_t noff = size - 1;
+        lfs_off_t index = lfs_ctz_index(lfs, &noff);
+        noff = noff + 1;
+        
+        // just copy out the last block if it is incomplete
+        if (noff != lfs->cfg->block_size) {
+            for (lfs_off_t i = 0; i < noff; i++) {
+                uint8_t data;
+                err = lfs_bd_read(lfs,
+                        NULL, rcache, noff-i,
+                        head, i, &data, 1);
+                if (err) {
                     return err;
                 }
 
-                if (i != skips-1) {
-                    err = lfs_bd_read(lfs,
-                            NULL, rcache, sizeof(nhead),
-                            nhead, 4*i, &nhead, sizeof(nhead));
-                    nhead = lfs_fromle32(nhead);
-                    if (err) {
-                        return err;
+                err = lfs_bd_prog(lfs,
+                        pcache, rcache, true,
+                        nblock, i, &data, 1);
+                if (err) {
+                    if (err == LFS_ERR_CORRUPT) {
+                        // This was a goto relocate, stop the for loop
+                        lfs->ctz_extend_next = ctz_extend_relocate;
+                        break;
                     }
+                    return err;
                 }
             }
 
             *block = nblock;
-            *off = 4*skips;
-            return 0;
+            *off = noff;
+            // Prepare the next call
+            lfs->ctz_extend_next = ctz_extend_done;
         }
 
-relocate:
-        LFS_DEBUG("Bad block at 0x%"PRIx32, nblock);
+        // Call next state
+       return lfs->ctz_extend_next(lfs, pcache, rcache, head, size, block, off, nblock);
+}
 
-        // just clear cache and try a new block
-        lfs_cache_drop(lfs, pcache);
+// the rest ..
+static int lfs_ctz_extend(lfs_t *lfs,
+        lfs_cache_t *pcache, lfs_cache_t *rcache,
+        lfs_block_t head, lfs_size_t size,
+        lfs_block_t *block, lfs_off_t *off) {
+
+    lfs_block_t nblock;
+    // alloc block
+    int err = ctz_extend_alloc(lfs, pcache, rcache, head, size, block, off, &nblock);
+    if (err) {
+        return err;
     }
 }
 #endif
